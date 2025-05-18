5d680a061eb7ee36e35b2cb2c5e6c3d446e142b3
PKRH <17289367+pkrh39@users.noreply.github.com>
2025-05-18 00:31:39 +0000

    Fix permissions for Chromium patch executable

--- src/patches/chromium.ts
+++ src/patches/chromium.ts
@@ -77,7 +77,7 @@ export async function patchChromiumApps(){
     fs.mkdirSync(path.join(bashPath, ".."), { recursive: true });
     fs.writeFileSync(bashPath, amdhelperChromiumBash(apps, global.disableGpuMode));
     await exec(`chmod +x ${escapePathSpaces(bashPath)}`);
-    await exec(`chmod 755 ${escapePathSpaces(bashPath)}`);
+    // await exec(`chmod 755 ${escapePathSpaces(bashPath)}`);
     await exec(`chown 0:0 ${escapePathSpaces(bashPath)}`);
 
     fs.writeFileSync(plistPath, amdhelperChromiumPlist);    
